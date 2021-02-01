ec43d4956cc33cecefbfdc042c5e6ead47d656f2
wimanshaherath <wimanshah@gmail.com>
2021-02-01 22:12:10 +0530

    started linked list

---
+++ LinkedList/linkedlist.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+class Node{
+  public:
+    int data;
+    Node* next;
+};
+
+/*insert a new node to front*/
+void push(Node** head,int new_data){
+    
+}
+
+/*insert a new node after a given node*/
+void add_after(Node** prevNode,int new_data){
+
+}
+/*insert a new node at the end*/
+void append(Node** head_ref, int new_data){
+
+}
+
+int main(){
+  std::cout<<'\n'<<std::string(50,'-')<<std::endl;
+  std::cout<<"\t\tLinked List"<<std::endl;
+  std::cout<<std::string(50,'-')<<std::endl;
+  
+
+
+
+}
